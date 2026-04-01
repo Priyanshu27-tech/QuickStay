64063c5daae60855bf1f09a667c5eaa9be06a002
Priyanshu27-tech <priyanshughosh365@gmail.com>
2026-04-01 16:11:38 +0530

    Update axios base URL to use environment variable

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const AppContext = createContext();
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const AppProvider = ({ children }) => {
   const [user, setUser] = useState(() => {
