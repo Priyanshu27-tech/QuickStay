9e50e02e74e867bf79666e469204f3b824d8c2b7
Priyanshu27-tech <priyanshughosh365@gmail.com>
2026-04-02 02:15:06 +0530

    Implement ChangeMapView for dynamic map updates
    Added ChangeMapView component to update map view based on location props.

--- client/src/components/LocationMap.jsx
+++ client/src/components/LocationMap.jsx
@@ -1,21 +1,33 @@
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
+import { useState, useEffect } from "react";
 import L from "leaflet";
 
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// Fix Leaflet marker icon issue
 const DefaultIcon = L.icon({
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+  iconSize: [25,41],
+  iconAnchor: [12,41]
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+const ChangeMapView = ({ coords }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if(coords){
+      map.setView([coords.lat, coords.lng], 13);
+    }
+  }, [coords]);
+
+  return null;
+};
+
 const LocationMarker = ({ setLocation }) => {
+
   const [position, setPosition] = useState(null);
 
   useMapEvents({
@@ -28,19 +40,24 @@ const LocationMarker = ({ setLocation }) => {
   return position ? <Marker position={position} /> : null;
 };
 
-const LocationMap = ({ setLocation }) => {
+const LocationMap = ({ location, setLocation }) => {
+
   return (
     <MapContainer
-      center={[22.5726, 88.3639]}   // Default location (India)
+      center={[22.5726, 88.3639]}
       zoom={12}
-      style={{ height: "250px", width: "100%" }}  // Reduced height
+      style={{ height: "250px", width: "100%" }}
     >
+
       <TileLayer
         attribution="© OpenStreetMap"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
+      <ChangeMapView coords={location} />
+
       <LocationMarker setLocation={setLocation} />
+
     </MapContainer>
   );
 };
