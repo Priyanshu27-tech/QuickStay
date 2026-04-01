0870ef39fb50b9db6c5786276bc419c706015661
Priyanshu27-tech <priyanshughosh365@gmail.com>
2026-04-02 02:44:07 +0530

    Remove 'city' field from hotel schema
    Removed the 'city' field from the hotel schema.

--- server/models/Hotel.js
+++ server/models/Hotel.js
@@ -4,7 +4,7 @@ const hotelSchema = new mongoose.Schema({
     name:        { type: String, required: true },
     address:     { type: String, required: true },
     contact:     { type: String, required: true },
-    city:        { type: String, required: true },
+   
 
     owner:       { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
