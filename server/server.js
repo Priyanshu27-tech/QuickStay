a036527957591a9cbd5b476e7cfac3a206403c19
Priyanshu27-tech <priyanshughosh365@gmail.com>
2026-04-01 15:56:07 +0530

    Update CORS origin for local development

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ connectCloudinary();
 const app = express();
 
 app.use(cors({
-    origin: ["https://quickstay-1-9z47.onrender.com"],
+    origin: ["http://localhost:5173"],
     credentials: true
 }));
 app.use(express.json());
