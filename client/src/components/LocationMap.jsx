af027979e09045e5d450f5daffbb6e7195b7acf9
Priyanshu27-tech <priyanshughosh365@gmail.com>
2026-04-02 02:25:50 +0530

    Fix formatting and improve marker icon handling

--- client/src/components/LocationMap.jsx
+++ client/src/components/LocationMap.jsx
@@ -5,11 +5,12 @@ import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+// Fix marker icon
 const DefaultIcon = L.icon({
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
-  iconSize: [25,41],
-  iconAnchor: [12,41]
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
@@ -18,7 +19,7 @@ const ChangeMapView = ({ location }) => {
   const map = useMap();
 
   useEffect(() => {
-    if(location){
+    if (location) {
       map.setView([location.lat, location.lng], 13);
     }
   }, [location]);
@@ -27,37 +28,38 @@ const ChangeMapView = ({ location }) => {
 };
 
 const LocationMarker = ({ setLocation }) => {
-
   const [position, setPosition] = useState(null);
 
   useMapEvents({
-    click(e){
-      setPosition(e.latlng);
-      setLocation(e.latlng);
+    click(e) {
+      const coords = {
+        lat: e.latlng.lat,
+        lng: e.latlng.lng
+      };
+
+      setPosition(coords);
+      setLocation(coords);
     }
   });
 
-  return position ? <Marker position={position}/> : null;
+  return position ? <Marker position={position} /> : null;
 };
 
 const LocationMap = ({ location, setLocation }) => {
-
   return (
     <MapContainer
       center={[22.5726, 88.3639]}
       zoom={12}
       style={{ height: "250px", width: "100%" }}
     >
-
       <TileLayer
         attribution="© OpenStreetMap"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <ChangeMapView location={location}/>
+      <ChangeMapView location={location} />
 
-      <LocationMarker setLocation={setLocation}/>
-
+      <LocationMarker setLocation={setLocation} />
     </MapContainer>
   );
 };
